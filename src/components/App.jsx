5e8d3913932fe8f8c27e91f045456f225043551a
Denys <k.denis.aa@gmail.com>
2023-01-22 15:40:57 +0200

    prettier change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,25 +9,25 @@ import friends from './FriendList/friends.json';
 import transactions from './TransactionHistory/transactions.json';
 
 export const App = () => {
-  return (
-    <>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
+	return (
+		<>
+			<Profile
+				username={user.username}
+				tag={user.tag}
+				location={user.location}
+				avatar={user.avatar}
+				stats={user.stats}
+			/>
 
-      <Statistics
-        title="Upload stats"
-        stats={data}
-      />
-      <Statistics stats={data} />
+			<Statistics
+				title="Upload stats"
+				stats={data}
+			/>
+			<Statistics stats={data} />
 
-      <FriendList friends={friends} />
+			<FriendList friends={friends} />
 
-      <TransactionHistory items={transactions} />
-    </>
-  );
+			<TransactionHistory items={transactions} />
+		</>
+	);
 };
